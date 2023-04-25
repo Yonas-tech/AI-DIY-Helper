014dddab6b448dca56c4886a91bc0993b6d2c30f
Yonas-coder <52844663+Yonas-coder@users.noreply.github.com>
2023-04-25 15:54:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import fetchData from './components/fetchData';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [responseData, setResponseData] = useState();
+  const [steps, setSteps] = useState('');
+
+  useEffect(() => {
+    console.log('Steps:')
+    console.log(steps)
+    console.log('Fetched Data:')
+    console.log(JSON.stringify(responseData));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [steps])
+
+  useEffect(async () => {
+    if(responseData){
+      setSteps(responseData.choices); //[0].message.content
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [responseData])
+
+  useEffect(async () => {
+    let data = await fetchData();
+    setResponseData(data)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
---
+++ src/components/fetchData.js
@@ -0,0 +1,31 @@
+const My_API_KEY = "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";
+// https://api.openai.com/v1/completions   for model: text-davinci-003
+// gpt-3.5-turbo
+// max_tokens: 7
+
+// For better deterministic model set 
+// temperature: 0.8
+// top_p: 
+
+
+async function fetchData() {
+    const response = await fetch ("https://api.openai.com/v1/chat/completions",{
+        method: "POST",
+        headers: {
+            Authorization:`Bearer ${My_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo-0301", 
+            messages: [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": "DIY steps for reverse osmosis?"}],
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+
+    return<></>
+}
+
+export default fetchData;
